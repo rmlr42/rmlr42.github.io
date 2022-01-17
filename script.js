33ad8cd9b55df0e458004be4daff061f768a5974
rmlr42 <96788020+rmlr42@users.noreply.github.com>
2022-01-17 05:36:41 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const images = [
   "assets/shot/1.png",
   "assets/shot/2.png",
-  "assets/shot/3.png",
+  "assets/shot/colage.png",
   "assets/shot/4.png",
   "assets/shot/5.png",
   "assets/shot/6.png",
@@ -21,7 +21,7 @@ const images = [
 len = images.length;
 i = 0;
 
-document.getElementById("arch_total").innerHTML = len
+document.getElementById("arch_total").innerHTML = len;
 
 document.getElementById("pos").onclick = function changer_f(){
   i += 1;
@@ -43,5 +43,23 @@ document.getElementById("pre").onclick= function changer_b(){
 }
 
 
+document.addEventListener('keydown', e => {
+  if(e.key === 'ArrowLeft'){
+    i -= 1;
+    if (i < 0) {
+      i = len - 1;
+    }
+    document.getElementById("arch_img").src = images[i];
+    document.getElementById("arch_number").innerHTML = i + 1;
+  }
+})
 
-
+document.addEventListener('keydown', e => {
+  if(e.key === 'ArrowRight'){
+    i += 1;
+    if (i > len - 1) {
+      i = 0;
+    }
+    document.getElementById("arch_img").src = images[i];
+    document.getElementById("arch_number").innerHTML = i + 1;
+  }})
